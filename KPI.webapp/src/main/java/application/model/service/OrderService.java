599240ac9720a081870da356dc95a4dbbd3967bc
Nastya Tychyna <43435254+ntchna@users.noreply.github.com>
2021-01-19 10:55:11 +0200

    Update OrderService.java

--- KPI.webapp/src/main/java/application/model/service/OrderService.java
+++ KPI.webapp/src/main/java/application/model/service/OrderService.java
@@ -16,7 +16,6 @@ public class OrderService {
         OrderDao dao = new OrderDao();
         dao.save(newOrder);
         dao.closeConnection();
-        System.out.println(newOrder);
         return newOrder +"Your order was completed!";
     }
 
@@ -24,7 +23,6 @@ public class OrderService {
         OrderDao dao = new OrderDao();
         List<Order> orders = dao.getAll();
         dao.closeConnection();
-        System.out.println(orders);
         if(orders == null|| orders.size()==0) throw new EmptyListException("No orders");
         return orders;
     }
@@ -33,7 +31,6 @@ public class OrderService {
         OrderDao dao = new OrderDao();
         Order order = dao.getByID(userID);
         if(order!=null){
-            System.out.println(order);
             return order;}
         else throw new NoSuchOrderException();
     }
@@ -46,38 +43,7 @@ public class OrderService {
         WeightDao weightDao = new WeightDao();
         Weight weight = weightDao.getByName(order.getWeight().getName());
         double amount = direction.getPrice()+(direction.getPrice()* dimension.getPrice())+(direction.getPrice()*weight.getPrice());
-        System.out.println(direction.getPrice());
-        System.out.println(dimension.getPrice());
-        System.out.println(weight.getPrice());
-        System.out.println(amount);
         return order;
     }
-    /*
-    public static void main(String[] args) throws MySQLException {
-        OrderService service = new OrderService();
-        service.getAllOrders();
-
-    }
-
-    public static void main(String[] args) throws MySQLException {
-        OrderService service = new OrderService();
-        service.getOrder(1);}
-
-    public static void main(String[] args) throws MySQLException {
-        OrderService service = new OrderService();
-        service.calculate(service.getOrder(1));
-    }
-
-    public static void main(String[] args) throws MySQLException {
-        OrderService service = new OrderService();
-        User user = new User(4);
-        BaggageType baggageType = new BaggageType(2);
-        Dimension dimencion = new Dimension(2.4);
-        Weight weight = new Weight("до 2.5 кг");
-        Direction direction = new Direction("Одеса");
-        Order order = new Order(user, baggageType, dimencion, weight, direction);
-        service.addOrder(order);
-        OrderDao dao = new OrderDao();
-        Order orderdao = dao.getByID(2);
-    }*/
+   
 }
